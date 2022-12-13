8bde4fc3450ec3d61c8e98c94a716ee96a2b5815
olgakahraman <ol.khrmn@gmail.com>
2022-12-13 14:21:18 +0300

    added js function getCurrentLocation

--- src/app.js
+++ src/app.js
@@ -48,3 +48,7 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
