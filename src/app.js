18ace00a9886a43ff5bfc2eff7c01dae49ab5766
olgakahraman <ol.khrmn@gmail.com>
2022-12-13 14:20:26 +0300

    added js functions displayWeatherCondition, searchCity and searchLocation

--- src/app.js
+++ src/app.js
@@ -25,3 +25,26 @@ function formatDate(date) {
 let h3 = document.querySelector("h3");
 let currentTime = new Date();
 h3.innerHTML = formatDate(currentTime);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#temperature-description").innerHTML =
+    response.data.weather[0].description;
+}
+function searchCity(city) {
+  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function searchLocation(position) {
+  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
